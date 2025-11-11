aaa78c2ac7471c26d104984bcdca03d9bfca63aa
eannaratone <105496244+eannaratone@users.noreply.github.com>
2025-11-11 19:28:59 +0100

    Refactor AdvShopApp test setup and methods

--- tests/AdvShopApp.py
+++ tests/AdvShopApp.py
@@ -1,60 +1,75 @@
 # -*- coding: utf-8 -*-
+import os
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 
-class AppDynamicsJob(unittest.TestCase):
+class AdvShopApp(unittest.TestCase):
     def setUp(self):
-        # AppDynamics will automatically override this web driver
-        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
+        # Chrome headless para CI (GitHub Actions)
+        opts = Options()
+        opts.add_argument("--headless=new")
+        opts.add_argument("--no-sandbox")
+        opts.add_argument("--disable-dev-shm-usage")
+        # (Opcional) si tu red corporativa hace MITM en SSL:
+        # opts.add_argument("--ignore-certificate-errors")
+
+        self.driver = webdriver.Chrome(options=opts)
+        self.driver.implicitly_wait(10)
+
+        # Toma la URL del entorno; fallback a la nueva app
+        self.base_url = os.getenv("APP_URL", "https://advantageonlineshopping.com").rstrip("/")
+
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_app_dynamics_job(self):
-        driver = self.driver
-        driver.get("http://zero.webappsecurity.com/index.html")
-        driver.find_element_by_link_text("Zero Bank").click()
-        driver.find_element_by_xpath("//li[@id='onlineBankingMenu']/div/strong").click()
-        driver.get("http://zero.webappsecurity.com/online-banking.html")
-        driver.find_element_by_xpath("//li[@id='feedback']/div").click()
-        driver.get("http://zero.webappsecurity.com/feedback.html")
-        driver.find_element_by_link_text("Zero Bank").click()
-        driver.get("http://zero.webappsecurity.com/index.html")
-        driver.find_element_by_id("online-banking").click()
-        driver.get("http://zero.webappsecurity.com/online-banking.html")
-    
+
+    def test_smoke_homepage_title(self):
+        d = self.driver
+        d.get(self.base_url)
+        # Smoke assertion básica: el título contiene "Advantage"
+        self.assertIn("Advantage", d.title)
+
+        # (Opcional) Más validaciones poco frágiles:
+        # Comprueba que hay al menos un link/categoría en la página
+        # Esto evita depender de selectores muy específicos de Angular.
+        links = d.find_elements(By.TAG_NAME, "a")
+        self.assertTrue(len(links) > 0, "No se encontraron enlaces en la página principal")
+
+    # Helpers actualizados a Selenium 4
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
+        try:
+            self.driver.find_element(how, what)
+            return True
+        except NoSuchElementException:
+            return False
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+        try:
+            _ = self.driver.switch_to.alert
+            return True
+        except NoAlertPresentException:
+            return False
+
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.driver.switch_to.alert
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
-        # To know more about the difference between verify and assert,
-        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
-        self.assertEqual([], self.verificationErrors)
+        try:
+            self.driver.quit()
+        finally:
+            # Mantén verificación de errores estilo unittest
+            self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
