cd3a6e21e1a3fc886a9c5979af3acaeaa33a91e3
eannaratone <105496244+eannaratone@users.noreply.github.com>
2025-11-11 16:39:14 +0100

    Update AppDynamicsJob to use Chrome headless

--- tests/AppDynamicsJob.py
+++ tests/AppDynamicsJob.py
@@ -1,60 +1,72 @@
 # -*- coding: utf-8 -*-
+import os
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
-        # AppDynamics will automatically override this web driver
-        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
+        # Chrome headless para CI
+        opts = Options()
+        opts.add_argument("--headless=new")
+        opts.add_argument("--no-sandbox")
+        opts.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=opts)
+        self.driver.implicitly_wait(10)
+
+        # Toma la URL de entorno (fallback a la tuya)
+        self.base_url = os.getenv("APP_URL", "http://zero.webappsecurity.com").rstrip("/")
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_app_dynamics_job(self):
-        driver = self.driver
-        driver.get("http://zero.webappsecurity.com/index.html")
-        driver.find_element_by_link_text("Zero Bank").click()
-        driver.find_element_by_xpath("//li[@id='onlineBankingMenu']/div/strong").click()
-        driver.get("http://zero.webappsecurity.com/online-banking.html")
-        driver.find_element_by_xpath("//li[@id='feedback']/div").click()
-        driver.get("http://zero.webappsecurity.com/feedback.html")
-        driver.find_element_by_link_text("Zero Bank").click()
-        driver.get("http://zero.webappsecurity.com/index.html")
-        driver.find_element_by_id("online-banking").click()
-        driver.get("http://zero.webappsecurity.com/online-banking.html")
-    
+        d = self.driver
+        d.get(f"{self.base_url}/index.html")
+        d.find_element(By.LINK_TEXT, "Zero Bank").click()
+        d.find_element(By.XPATH, "//li[@id='onlineBankingMenu']/div/strong").click()
+        d.get(f"{self.base_url}/online-banking.html")
+        d.find_element(By.XPATH, "//li[@id='feedback']/div").click()
+        d.get(f"{self.base_url}/feedback.html")
+        d.find_element(By.LINK_TEXT, "Zero Bank").click()
+        d.get(f"{self.base_url}/index.html")
+        d.find_element(By.ID, "online-banking").click()
+        d.get(f"{self.base_url}/online-banking.html")
+
+    # Helpers actualizados a Selenium 4
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
+        try:
+            self.driver.find_element(how, what)
+            return True
+        except NoSuchElementException:
+            return False
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+        try:
+            _ = self.driver.switch_to.alert
+            return True
+        except NoAlertPresentException:
+            return False
+
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.driver.switch_to.alert
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
-        # To know more about the difference between verify and assert,
-        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
-        self.assertEqual([], self.verificationErrors)
+        # Cierra el navegador y valida errores
+        try:
+            self.driver.quit()
+        finally:
+            self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
